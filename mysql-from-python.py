e0756d4e504f04228401ebedff5b56ff3640c4b8
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-03-11 21:38:47 +0000

    Create a table at mysql-from-python.py file

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get the username from the Cloud9 workspace
@@ -12,10 +13,10 @@ connection = pymysql.connect(host='localhost',
                              db='Chinook')
 
 try:
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "SELECT * FROM Genre;"
-        cursor.execute(sql)
-        for row in cursor:
-            print(row)
+    with connection.cursor() as cursor:
+        cursor.execute("""CREATE TABLE IF NOT EXISTS
+                          Friends(name char(20), age int, DOB datetime);""")
+        # Note that the above will still display a warning (not error) if the
+        # table already exists
 finally:
     connection.close()
