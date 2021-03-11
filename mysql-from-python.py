fbb37172121a000539000d16e605bce1dc6f2663
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-03-11 22:14:17 +0000

    Create a new Table Friends and insert new values

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -14,9 +14,8 @@ connection = pymysql.connect(host='localhost',
 
 try:
     with connection.cursor() as cursor:
-        cursor.execute("""CREATE TABLE IF NOT EXISTS
-                          Friends(name char(20), age int, DOB datetime);""")
-        # Note that the above will still display a warning (not error) if the
-        # table already exists
+        row = ("Bob", 21, "1990-02-06 23:04:56")
+        cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        connection.commit()
 finally:
     connection.close()
