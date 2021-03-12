6f6f44b9f0a311f859029fdc64f7d25650cddb95
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-03-12 14:23:37 +0000

    Alternate Update on the friends table using %s

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -12,7 +12,8 @@ connection = pymysql.connect(host='localhost',
                              db='Chinook')
 try:
     with connection.cursor() as cursor:
-        cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'bob';")
+        cursor.execute("UPDATE Friends SET age = %s WHERE name = %s;",
+                       (23, 'Bob'))
         connection.commit()
 finally:
     connection.close()
